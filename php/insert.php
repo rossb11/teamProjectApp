a1ab0e2d6211a87e4562cb0ec08b30a1ffdbb8b1
jackdunne2345 <34764297+jackdunne2345@users.noreply.github.com>
2020-12-02 03:00:37 +0000

    Add files via upload

--- php/insert.php
+++ php/insert.php
@@ -1,12 +1,14 @@
 <?php
     require_once 'connect.php';
-    $dbLocation=mysqli_real_escape_string($conn,$_REQUEST['dbLocation']);
+    $dbTable=mysqli_real_escape_string($conn,$_REQUEST['dbTable']);
     $name=mysqli_real_escape_string($conn,$_REQUEST['name']);
+    $cat=mysqli_real_escape_string($conn,$_REQUEST['cat']);
+    $location=mysqli_real_escape_string($conn,$_REQUEST['location']);
     $mail=mysqli_real_escape_string($conn,$_REQUEST['email']);
     $pnum=mysqli_real_escape_string($conn,$_REQUEST['phone']);
     $dec=mysqli_real_escape_string($conn,$_REQUEST['dec']);
     
-    $sql="INSERT INTO found (itemDec,usserName, phone, email) VALUE ('$dec','$name','$pnum','$mail')";
+    $sql="INSERT INTO $dbTable (category,itemDec,usserName, phone, email,location) VALUE ('$cat','$dec','$name','$pnum','$mail','$location')";
 
 
     if(mysqli_query($conn, $sql)){
@@ -15,7 +17,7 @@
         print("failed");
     }
     mysqli_close($conn);
-
+    
    
    echo "<script>location.href='../index.html'</script>";
 ?>
