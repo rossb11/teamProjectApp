e48303aa0419ac07ed9362848255056c11e7b7c1
rossb11 <56341485+rossb11@users.noreply.github.com>
2020-12-08 15:06:13 +0000

    Changes

--- js/foundForm.js
+++ js/foundForm.js
@@ -1,5 +1,5 @@
 function foundForm(){ 
-
+    //object 
     const foundListing = {
         name:  $("input#name").val(),
         email: $("input#email").val(),
@@ -8,28 +8,29 @@ function foundForm(){
         location: $("textarea#Location").val(),
         itemDescription: $("textarea#ItemDescription").val()
     }
-    
+    //For coordinates
     const geocoder = new google.maps.Geocoder();
 
     geocoder.geocode( { 'address': foundListing.location}, function(results, status) {
         if (status == 'OK') {
-            
+            //Adds coordinates to database longitude and latitude 
             foundListing.longitude = results[0].geometry.location.lat();
             foundListing.latitude = results[0].geometry.location.lng();
 
+            //if statement throws toast error if fields not filled in 
             if(foundListing.name === "" || foundListing.email === "" || foundListing.phone === "" || foundListing.category === ""
             || foundListing.location === "" || foundListing.itemDescription === ""){
                 toast.error("Form is invalid");
-            }else{
+            } else{ 
                 $.ajax({
                     url: "/php/found.php",
                     type: "POST",
                     data: foundListing,
                     cache: false,
-                    success: function () {
+                    success: function () {  //success message
                         toast.success("Saved to Database!");
                     },
-                    error: function () {
+                    error: function () { //error message
                         toast.error("There was an error!");
                     }
                 });
